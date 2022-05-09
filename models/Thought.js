03b53b93d39f407b9d48aa7a596323ded375f277
jrwj97 <jrwj97@aol.com>
2022-05-09 17:02:19 -0400

    finished user and thought models

--- models/Thought.js
+++ models/Thought.js
@@ -2,8 +2,15 @@ const { Schema, model } = require("mongoose");
 const dateFormat = require("../utils/dateFormat");
 
 const ReactionSchema = new Schema({
-  reactionId: {},
-  reactionBody: {},
+  reactionId: {
+    type: Schema.Types.ObjectId,
+    default: () => new Types.ObjectId()
+  },
+  reactionBody: {
+    type: String,
+    required: true,
+    maxLength: 280
+  },
   username: {
     type: String,
     required: true,
@@ -13,15 +20,23 @@ const ReactionSchema = new Schema({
     default: new Date(),
     get: (createdAtVal) => dateFormat(createdAtVal),
   },
-});
+},
+{
+  toJSON: {
+    virtuals: true,
+    getters: true
+  },
+  id: false
+}
+);
 
 const ThoughtSchema = new Schema(
   {
     thoughtText: {
       type: String,
       required: true,
-      minimumLength: 1,
-      maximumLength: 128,
+      minLength: 1,
+      maxLength: 128,
     },
     createdAt: {
       type: Date,
@@ -32,7 +47,7 @@ const ThoughtSchema = new Schema(
       type: String,
       required: true,
     },
-    reactions: [reactionSchema],
+    reactions: [ReactionSchema],
   },
   {
     toJSON: {
@@ -43,23 +58,10 @@ const ThoughtSchema = new Schema(
   }
 );
 
-const UserSchema = new Schema({
-  username: {
-    type: String,
-    unique: true,
-    required: true,
-    trim: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    match: /.+\@.+\..+/,
-  },
-  thoughts: {
+ThoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
 
-  },
-  friends: {
-    
-  }
-})
+const Thought = model("Thought", ThoughtSchema);
+
+module.exports = Thought
